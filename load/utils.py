39f1a57b97beaa82ee3a4339c6f866c1ca16f885
SunnerLi <a6214123@gmail.com>
2017-10-03 13:22:43 +0800

    speed up the to_categorical and maybe correct

--- load/utils.py
+++ load/utils.py
@@ -5,13 +5,14 @@ import numpy as np
     This function provide to_categorical which just like keras.utils
 """
 
-def to_categorical_4d(_input_tensor, pallete=None):
+def to_categorical_4d(_input_tensor, pallete=None, verbose=1):
     """
         Change the representation to the one-hot format
         Just like the keras' to_categorical but in 4D shae
 
         Arg:    _input_tensor   - The input tensor want to change into one-hot format
                 pallete         - The pallete object (default is None)
+                verbose         - Show the percentage of completing                
         Ret:    The result tensor in 4D shape [batch_num, height, width, class_num]
     """ 
     # Build the pallete and create the zero array
@@ -19,17 +20,10 @@ def to_categorical_4d(_input_tensor, pallete=None):
     batch, height, width, channel = np.shape(_input_tensor)
     _result_tensor = np.zeros([batch, height, width, len(pallete)])
 
-    """
-    # Build index tensor
-    encode_map = _input_tensor[:, :, :, 0] * 100 + _input_tensor[:, :, :, 1] * 10 + _input_tensor[:, :, :, 2]
-    encode_map = np.vectorize(pallete.get)(encode_map)
-
     # Fill the corresponding index as 1
-    for _c in range(len(pallete)):
-        _result_tensor[encode_map == _c, _c] = 1
-    return _result_tensor, pallete
-    """
     for i in range(batch):
+        if verbose == 1:
+            print('to_categorical_4d finish: ', i/batch)
         for j in range(height):
             for k in range(width):
                 _result_tensor[i][j][k][pallete[tuple(_input_tensor[i, j, k, :].tolist())]] = 1
@@ -50,26 +44,11 @@ def to_categorical_4d_reverse(_input_tensor, pallete):
     decode_map = np.argmax(_input_tensor, axis=-1)
     batch, height, width, channel = np.shape(_input_tensor)        
     _result_tensor = np.zeros([batch, height, width, 3])    
-    # decode_map = np.vectorize(reverse_pallete.get)(decode_map)
 
-    """
-    # Decode the index to the original RGB color intensity
-    _result_tensor[:, :, :, 2] = decode_map[:, :, :] % 2
-    decode_map[:, :, :] = (decode_map[:, :, :] - _result_tensor[:, :, :, 2]) / 10
-    _result_tensor[:, :, :, 1] = decode_map[:, :, :] % 2
-    decode_map[:, :, :] = (decode_map[:, :, :] - _result_tensor[:, :, :, 1]) / 10
-    _result_tensor[:, :, :, 0] = decode_map[:, :, :] % 2
-    
-    for i in range(batch):
-        print(i)
-        for j in range(height):
-            for k in range(width):
-                _result_tensor[i][j][k] = np.asarray(reverse_pallete[decode_map[i][j][k]])
-    """
-    _result_tensor = np.asarray(np.vectorize(reverse_pallete.get)(decode_map))
-    _result_tensor = np.reshape(_result_tensor, [batch, height, width, 3])
-
-    return _result_tensor
+    # Mapping into origin color
+    _result_tensor = np.reshape(np.copy(decode_map), [-1])
+    _result_tensor = np.asarray(np.vectorize(reverse_pallete.get)(_result_tensor)).T
+    return np.reshape(_result_tensor, [batch, height, width, 3])
 
 def __buildPallete(_input_tensor, pallete):
     """
@@ -93,19 +72,7 @@ def __buildPallete(_input_tensor, pallete):
     if len(np.shape(_input_tensor)) != 4:
         print('< ear_pen >  error: The shape of tensor should be 4!')
         exit()
-    # for _slice in _input_tensor:
-    #     encode_map = _slice[:, :, 0] * 100 + _slice[:, :, 1] * 10 + _slice[:, :, 2]
-    #     counter = Counter(np.reshape(encode_map, [-1]))
-    #     for key in counter:
-    #         if not key in pallete:
-    #             pallete[key] = len(pallete)
-
-    encode_map = np.reshape(_input_tensor, [-1, 3])
-    for i in range(len(encode_map)):
-        _key = tuple(encode_map[i].tolist())
-        if not _key in pallete:
-            pallete[_key] = len(pallete)
-    #     if i % 1000000 == 0:
-    #         print(float(i) / len(encode_map))
-    # print(pallete)
+    unique_colors = np.unique(np.reshape(_input_tensor, [-1, 3]), axis=0)
+    for i in range(len(unique_colors)):
+        pallete[tuple(unique_colors[i].tolist())] = len(pallete)
     return pallete
